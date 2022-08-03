fba2a5b4542cab3292e515104ff1b84cc2225553
willybc <willybc06@gmail.com>
2022-08-02 23:59:21 -0300

    MySQL Exercises

---
+++ MySQL/consultas_clientes.sql
@@ -0,0 +1,9 @@
+/* Una empresa tiene registrados a sus clientes en una tabla llamada clientes. 
+   Tambien tiene una tabla "provincias" donde registra los nombres de las provincias.
+   En base a los datos solo tenes que copiarte las sentencias de creacion e insercion*/
+
+-- En que provincias no tenemos clientes?
+
+
+-- Que provincias tienen clientes? Pero sin repetir el nombre de la provincia
+
