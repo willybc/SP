f25d7c4d379a514c7a06dcdb36035a2d843e0e0d
willybc <willybc06@gmail.com>
2022-08-03 00:16:43 -0300

    MySQL Exercises Completed

--- MySQL/consultas_clientes.sql
+++ MySQL/consultas_clientes.sql
@@ -3,7 +3,13 @@
    En base a los datos solo tenes que copiarte las sentencias de creacion e insercion*/
 
 -- En que provincias no tenemos clientes?
-
+SELECT provincias.codigo, provincias.nombre FROM provincias
+	LEFT JOIN clientes
+	ON clientes.codigoProvincia = provincias.codigo
+    WHERE clientes.codigoProvincia IS null
 
 -- Que provincias tienen clientes? Pero sin repetir el nombre de la provincia
-
+SELECT DISTINCT p.codigo, p.nombre FROM provincias AS p
+    LEFT JOIN clientes AS c
+    ON c.codigoProvincia = p.codigo
+    WHERE c.codigoProvincia
